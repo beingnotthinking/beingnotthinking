0e8b67ed178821ec52745ccf918f38327b6a2083
pfarwick <patrick.farwick@gmail.com>
2022-12-27 18:43:18 +0800

    m

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,17 +1,8 @@
 const navigation = require('@11ty/eleventy-navigation')
 const dates = require('./utilities/filters/dates')
 const helpers = require('./utilities/filters/helpers')
-const insta = require("./netlify/functions/instagram")
 const path = require('path')
 const axios = require("axios");
-const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
-
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
-    name: "instagram", // The serverless function name from your permalink object
-    functionsDir: "./netlify/functions/",
-  });
-};
 
 module.exports = config => {
 
